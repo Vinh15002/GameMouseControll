8772d316fa0edb2eaa11cfeffba389bfc1aeda7a
Vinh <vvvinhtran@gmail.com>
2024-09-28 17:00:30 +0700

    firstCommit

---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float jetpackForce = 15f;
+
+    private Rigidbody2D _rb;
+
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+
+
+       
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rb.velocity = new Vector2(0, jetpackForce);
+        }
+    }
+}
